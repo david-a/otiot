f7f5c9e6ddb8a08ec863082500ab3b75cc3a8499
David Avikasis <david.avikasis@gmail.com>
2025-11-26 01:59:06 +0200

    fix dev mode

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,18 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import type { UserConfig } from 'vite';
+import { fileURLToPath } from 'url';
+import { dirname, resolve } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const config: UserConfig = {
-  plugins: [sveltekit()]
+  plugins: [sveltekit()],
+  server: {
+    fs: {
+      allow: [resolve(__dirname, 'content')]
+    }
+  }
 };
 
 export default config;
